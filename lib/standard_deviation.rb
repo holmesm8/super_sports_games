e8545a510a2398ee27b216c7f420b47c123d0779
Matt Holmes <holmesm8@gmail.com>
2019-11-21 13:43:31 -0700

    Update formulas for mean

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -6,8 +6,18 @@ ages.each do |x|
   added_total += x
 end
 
-puts added_total
+number_of_ages = ages.count
+calculate_mean = added_total / ages.count.to_f
 
+calculate_mean = calculate_mean.to_f
+
+number_minus_avg = []
+
+ages.each do |x|
+  number_minus_avg -= 26.6
+end
+
+puts number_minus_avg
 
 
 
