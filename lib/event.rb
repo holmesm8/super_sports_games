f61f3e4286a531ad3b26b6db47de3741142d937d
Matt Holmes <holmesm8@gmail.com>
2019-11-21 15:47:08 -0700

    Update event class

--- lib/event.rb
+++ lib/event.rb
@@ -3,21 +3,29 @@ class Event
 
   def initialize(name, ages)
     @name = name
-    @ages = ages
+    @ages =ages
   end
 
   def max_age
+    @ages.max
   end
 
   def min_age
+    @ages.min
   end
 
   def average_age
+    subtotal = 0
+    @ages .each do |x|
+      subtotal += x
+    end
+    num_of_ages = @ages.count.to_f
+    subtotal / num_of_ages
   end
 
   def standard_deviation_age
+
   end
-
-
+require "pry"; binding.pry
 
 end
