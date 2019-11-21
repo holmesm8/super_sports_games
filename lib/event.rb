70f6b159289dd30e1573f39f8e00665abe904545
Matt Holmes <holmesm8@gmail.com>
2019-11-21 15:00:31 -0700

    Add event class

---
+++ lib/event.rb
@@ -0,0 +1,23 @@
+class Event
+  attr_reader :name, :ages
+
+  def initialize(name, ages)
+    @name = name
+    @ages = ages
+  end
+
+  def max_age
+  end
+
+  def min_age
+  end
+
+  def average_age
+  end
+
+  def standard_deviation_age
+  end
+
+
+
+end
